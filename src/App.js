5f940a75d626e146fdc1e60f168f2471af6e7271
Travis Craft <tlcraft@users.noreply.github.com>
2022-08-06 22:06:01 -0600

    Add simple hello world example.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const helloElement = <h1>Hello world!</h1>;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        { helloElement }
         <a
           className="App-link"
           href="https://reactjs.org"
