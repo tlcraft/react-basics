1e9165b4f3aef94ed8eb431d53ae9a6a7241c15e
Travis Craft <tlcraft@users.noreply.github.com>
2022-08-13 13:56:16 -0600

    Add interface.

--- src/components/name-display.tsx
+++ src/components/name-display.tsx
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import React from 'react';
 
-class NameDisplay extends Component<any> {
+interface NameDisplayProps {
+    name: string;
+}
+
+class NameDisplay extends Component<NameDisplayProps> {
     render() {
         const { name } = this.props;
         return <p>{name}</p>;
