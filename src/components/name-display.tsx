9505e0eb04771bc592ae16c5bf79bee71930d7dd
Travis Craft <tlcraft@users.noreply.github.com>
2022-08-13 13:49:33 -0600

    Update name component.

--- src/components/name-display.tsx
+++ src/components/name-display.tsx
@@ -1,8 +1,11 @@
 import { Component } from "react";
+import React from 'react';
 
-class NameDisplay extends Component {
+class NameDisplay extends Component<any> {
     render() {
         const { name } = this.props;
         return <p>{name}</p>;
     }
 }
+
+export default NameDisplay;
