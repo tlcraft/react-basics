3e90340b3dbb79a5cdc5bcf89af0949f0cdc311b
Travis Craft <tlcraft@users.noreply.github.com>
2022-08-29 17:13:15 -0600

    Add static page example.

---
+++ src/static-page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function StaticPage() {
+    return (
+        <>  
+            <h2>Reasons why I'm excited to learn React:</h2>
+            <ol>
+                <li>React is popular</li>
+                <li>Components are great</li>
+            </ol>
+        </>
+    )
+}
+
+export default StaticPage;
