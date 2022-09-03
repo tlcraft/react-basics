71acb756a72c24d5c4d19dd815a4096958ec07be
Travis Craft <tlcraft@users.noreply.github.com>
2022-09-03 14:46:02 -0600

    Add header component.

---
+++ src/components/static/header.tsx
@@ -0,0 +1,26 @@
+import logo from '../../logo.svg';
+import React from 'react';
+
+function Header() {
+    const helloElement = <h1>Hello world!</h1>;
+    return (
+        <header className="App-header">
+            <nav>
+                <img src={logo} className="App-logo" alt="logo" />
+            </nav>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            { helloElement }
+            <a  className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+            </a>
+        </header>
+    )
+}
+
+export default Header;
