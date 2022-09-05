a5c0e037db1cec4f0f36d2bf8f5dd37610ee9c2c
Travis Craft <tlcraft@users.noreply.github.com>
2022-09-05 12:28:16 -0600

    Use header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import increment from './services/counter';
 import React, { useState } from 'react';
-import NameDisplay from './components/name-display';
-import List from './components/static/list';
-import StaticPage from './static-page';
+import increment from './services/counter';
 import Footer from './components/static/footer';
+import Header from './components/static/header';
+import List from './components/static/list';
+import NameDisplay from './components/name-display';
+import StaticPage from './static-page';
 
 function App() {
-  const helloElement = <h1>Hello world!</h1>;
   const [count, setCount] = useState(0);
   const list = ["Released in 2013", "Maintained by Meta (Facebook)", "Created by Jordan Walke"];
 
@@ -25,32 +24,16 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <nav>
-          <img src={logo} className="App-logo" alt="logo" />
-        </nav>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        { helloElement }
-        <button onClick={
+      <Header />
+      <button onClick={
             () => {
               const count = increment(); 
               setCount(count);
             }
           }>
           Increment Count
-        </button>
-        <p>{count}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </button>
+      <p>{count}</p>
       <NameDisplay name="John Doe"></NameDisplay>
       <h2>Fun Facts About React</h2>
       <List items={list}></List>
