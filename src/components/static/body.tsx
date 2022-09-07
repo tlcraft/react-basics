39d379628fc7d9652dfaaa5caf84f46b0bb76164
Travis Craft <tlcraft@users.noreply.github.com>
2022-09-06 19:02:11 -0600

    Add body component.

---
+++ src/components/static/body.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import increment from '../../services/counter';
+import List from './list';
+import NameDisplay from '../../components/name-display';
+
+function Body() {
+    const [count, setCount] = useState(0);
+    const list = ["Released in 2013", "Maintained by Meta (Facebook)", "Created by Jordan Walke"];
+  
+    const nav = (
+      <nav>
+          <h1>website</h1>
+          <ul>
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+          </ul>
+      </nav>
+    )
+    return (
+        <>      
+            <button onClick={
+                    () => {
+                    const count = increment(); 
+                    setCount(count);
+                    }
+                }>
+                Increment Count
+            </button>
+            <p>{count}</p>
+            <NameDisplay name="John Doe"></NameDisplay>
+            <h2>Fun Facts About React</h2>
+            <List items={list}></List>
+            <p>Nav Example: </p>
+            { nav }
+        </>
+    )
+}
+
+export default Body;
