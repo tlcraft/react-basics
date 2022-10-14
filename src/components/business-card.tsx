7055b2faca0572b0f375bc282c055e20f291675e
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-14 16:59:11 -0600

    Add components to parent.

--- src/components/business-card.tsx
+++ src/components/business-card.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import CardAbout from './business-card/card-about';
+import CardFooter from './business-card/card-footer';
+import CardInfo from './business-card/card-info';
+import CardInterests from './business-card/card-interests';
 
 function BusinessCard() {
-    return <p>Placeholder</p>;
+    return (
+        <>
+            <CardInfo />
+            <CardAbout />
+            <CardInterests />
+            <CardFooter />
+        </>
+    );
 }
 
 export default BusinessCard;
