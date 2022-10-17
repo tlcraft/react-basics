7eb70977374bf6ef5a856d91cac302ae60cbf0d6
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-16 19:38:52 -0600

    Add profile icons.

--- src/components/business-card/card-footer.tsx
+++ src/components/business-card/card-footer.tsx
@@ -1,7 +1,14 @@
+import './card-footer.css';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function CardFooter() {
-    return <p>Footer</p>;
+    return <div className="profiles">
+        <FontAwesomeIcon icon={brands('twitter')} />
+        <FontAwesomeIcon icon={brands('github')} />
+        <FontAwesomeIcon icon={brands('linkedin')} />
+    </div>;
 }
 
 export default CardFooter;
