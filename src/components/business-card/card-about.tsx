464f4cf0217b290b0d3a74dd68a0b55f24b74cf1
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-23 11:05:16 -0600

    Add styling and sample text.

--- src/components/business-card/card-about.tsx
+++ src/components/business-card/card-about.tsx
@@ -1,7 +1,8 @@
+import './card-about.css';
 import React from 'react';
 
 function CardAbout() {
-    return <p>About</p>;
+    return <p className="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>;
 }
 
 export default CardAbout;
