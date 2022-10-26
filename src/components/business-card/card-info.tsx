8ef738588adc31d77bf07e8468670c66442094d0
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-26 16:29:26 -0600

    Add image to card.

--- src/components/business-card/card-info.tsx
+++ src/components/business-card/card-info.tsx
@@ -1,8 +1,10 @@
+import './card-info.css';
 import React from 'react';
 
 function CardInfo() {
     return (
         <div>
+            <img src='../../nubelson-fernandes-UcYBL5V0xWQ-unsplash.jpg' />
             <p>Dev Name</p>
             <p>Position Title</p>
         </div> 
