63bd70bd935d57f517a4f18e8288a1898ff399f6
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-30 14:55:06 -0600

    Add titles, add padding.

--- src/components/business-card/card-interests.tsx
+++ src/components/business-card/card-interests.tsx
@@ -2,7 +2,12 @@ import './card-interests.css';
 import React from 'react';
 
 function CardInterests() {
-    return <p className="interests">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>;
+    return (
+        <>
+            <h3 className="interests">Interests</h3>
+            <p className="interests">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>    
+        </>
+    );
 }
 
 export default CardInterests;
