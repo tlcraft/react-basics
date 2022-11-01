64cfe55390cb28fff5ae55fdfd176b162084458f
Travis Craft <tlcraft@users.noreply.github.com>
2022-10-31 18:56:59 -0600

    Edit info section.

--- src/components/business-card/card-info.tsx
+++ src/components/business-card/card-info.tsx
@@ -5,7 +5,7 @@ function CardInfo() {
     return (
         <div>
             <img src='../../nubelson-fernandes-UcYBL5V0xWQ-unsplash.jpg' />
-            <p>Dev Name</p>
+            <p>Name</p>
             <p>Position Title</p>
         </div> 
     );
