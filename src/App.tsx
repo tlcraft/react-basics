d72e17a1f0cb8c01ceaa26b2407502dc6a54945e
Travis Craft <tlcraft@users.noreply.github.com>
2022-11-05 19:45:14 -0600

    Add browser router.

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import Footer from './components/static/footer';
 import Header from './components/static/header';
 import StaticPage from './components/static-page';
 import BusinessCard from './components/business-card';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-      <StaticPage />
-      <BusinessCard />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
