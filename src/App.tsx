10a49424a4d82b647dd1035926776c9f17edf033
Travis Craft <tlcraft@users.noreply.github.com>
2022-11-08 16:28:30 -0700

    Add initial route paths.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,40 @@ import Footer from './components/static/footer';
 import Header from './components/static/header';
 import StaticPage from './components/static-page';
 import BusinessCard from './components/business-card';
-import { BrowserRouter } from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
 
 function App() {
+
+  const navigate = useNavigate();
+  const navigateToStaticPage = () => {
+      navigate('/');
+  };
+
+  const navigateToBody = () => {
+    navigate('/body');
+  }
+
+  const navigateToCard = () => {
+    navigate('/card');
+  };
+
+  const headerProps = {
+    navigateToStaticPage: navigateToStaticPage,
+    navigateToBody: navigateToBody,
+    navigateToCard: navigateToCard
+  };
+
   return (
-    <BrowserRouter>
       <div className="App">
-        <Header />
-        <Body />
+        <Header {...headerProps}/>
+        <Routes>
+            <Route path="/" element={<StaticPage />} />
+            <Route path="/card" element={<BusinessCard />} />
+            <Route path="/body" element={<Body />} />
+        </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
   );
 }
 
