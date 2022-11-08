ac1bd571f76f9afd35d3d5f9a9523457b33b7b18
Travis Craft <tlcraft@users.noreply.github.com>
2022-11-07 18:33:22 -0700

    Add router to index.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootNode = document.getElementById('root');
 
@@ -10,7 +11,9 @@ if (rootNode) {
   const root = ReactDOM.createRoot(rootNode);
   root.render(
     <React.StrictMode>
-      <App />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
     </React.StrictMode>
   );
 
