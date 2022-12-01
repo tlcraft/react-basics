6be1b0c7b43b33e6ff37f12deeb4d40a4f39ce3e
Travis Craft <tlcraft@users.noreply.github.com>
2022-11-30 21:16:30 -0700

    Add initial hero component.

---
+++ src/components/vrbo/vrbo-hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VrboHero() {
+    return (
+        <div className="vrbo-hero">
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities all without leaving home.</p>
+        </div>
+    )
+}
+
+export default VrboHero;
