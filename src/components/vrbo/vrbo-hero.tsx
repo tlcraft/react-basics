4d47ec13d990450f40516086ed4be701308f8d4b
Travis Craft <tlcraft@users.noreply.github.com>
2022-12-02 17:36:17 -0700

    Add css.

--- src/components/vrbo/vrbo-hero.tsx
+++ src/components/vrbo/vrbo-hero.tsx
@@ -1,11 +1,12 @@
+import './vrbo-hero.css';
 import React from 'react';
 
 function VrboHero() {
     return (
-        <div className="vrbo-hero">
+        <section className="vrbo-hero">
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities all without leaving home.</p>
-        </div>
+        </section>
     )
 }
 
