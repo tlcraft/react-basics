911804781791f5f5fdceb372f70b082b8b7d7733
Travis Craft <tlcraft@users.noreply.github.com>
2022-12-04 11:41:52 -0700

    Display hero image.

--- src/components/vrbo/vrbo-hero.tsx
+++ src/components/vrbo/vrbo-hero.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 function VrboHero() {
     return (
         <section className="vrbo-hero">
+            <img src="../../hero.png" />
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities all without leaving home.</p>
         </section>
