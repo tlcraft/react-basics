3dc9d5d6db05425df74d3f18a02587a1a58c5ef7
Travis Craft <tlcraft@users.noreply.github.com>
2022-12-12 16:23:27 -0700

    Add initial card component.

---
+++ src/components/vrbo/vrbo-card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function VrboCard() {
+    return (
+        <section className="vrbo-card">
+            <img className="card-photo" src="" alt='Katie Zaferes'/>
+            <div>Rating</div>
+            <h1 className="card-description">Life lessons with Katie Zaferes</h1>
+            <p className="card-price"><span className="price">From $136</span><span>/ person</span></p>
+        </section>
+    )
+}
+
+export default VrboCard;
