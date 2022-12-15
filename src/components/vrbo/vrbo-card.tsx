09dbdf637b0b723bd991830f9226e5fb14fc714f
Travis Craft <tlcraft@users.noreply.github.com>
2022-12-15 15:48:53 -0700

    Update image.

--- src/components/vrbo/vrbo-card.tsx
+++ src/components/vrbo/vrbo-card.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function VrboCard() {
     return (
         <section className="vrbo-card">
-            <img className="card-photo" src="" alt='Katie Zaferes'/>
+            <img className="card-photo" src="../../katie-zaferes.png" alt='Katie Zaferes'/>
             <div>Rating</div>
             <h1 className="card-description">Life lessons with Katie Zaferes</h1>
             <p className="card-price"><span className="price">From $136</span><span>/ person</span></p>
