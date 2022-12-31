a31cd45a681f1b43188d6b148e7251ab3472e716
Travis Craft <tlcraft@users.noreply.github.com>
2022-12-31 11:54:19 -0700

    Pass in card props.

--- src/components/vrbo/vrbo.tsx
+++ src/components/vrbo/vrbo.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import VrboCard from './vrbo-card';
 import VrboHero from './vrbo-hero';
 import VrboNav from './vrbo-nav';
+import { CardProps } from './vrbo-card-props';
 
 function Vrbo() {
+    const cardProps: CardProps = {
+        imageSource: "../../katie-zaferes.png",
+        alternateText: "Katie Zaferes",
+        rating: "5.0",
+        ratingCount: 6,
+        country: "USA",
+        title: "Life lessons with Katie Zaferes",
+        price: "$136"
+    };
+
     return (
         <>
             <VrboNav />
             <VrboHero />
-            <VrboCard />
+            <VrboCard {...cardProps } />
         </>
     )
 }
