b8d10f36f205bd521b7a241397d6d4fc0e9435d2
Travis Craft <tlcraft@users.noreply.github.com>
2023-01-16 13:17:28 -0700

    Add badge logic.

--- src/components/vrbo/vrbo-card.tsx
+++ src/components/vrbo/vrbo-card.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { CardProps } from './vrbo-card-props';
 
 function VrboCard(props: CardProps) {
+    const badgeText = getBadgeText(props);
     return (
         <div className="vrbo-card">
-            { props.openSpots === 0 && <div className="badge">SOLD OUT</div> }
+            { badgeText && <div className="badge">{badgeText}</div> }
             <img className="card-photo" src={props.imageSource} alt={props.alternateText} />
             <div className="card-stats">
                 <img className="card-star" src="../../star.png" alt="star icon" />
@@ -17,6 +18,16 @@ function VrboCard(props: CardProps) {
             <p><span className="bold">From {props.price}</span><span> / person</span></p>
         </div>
     )
+}
+
+function getBadgeText(props: CardProps): string {
+    if (props.openSpots === 0) {
+        return "SOLD OUT";
+    } else if (props.location === "Online") {
+        return "ONLINE";
+    }
+
+    return "";
 }
 
 export default VrboCard;
