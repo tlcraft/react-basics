ca3c177780782e09b0ea35e2647095959bba6330
Travis Craft <tlcraft@users.noreply.github.com>
2023-01-25 17:04:31 -0700

    Add initial header.

---
+++ src/components/meme/meme-header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MemeHeader() {
+    return (
+        <header>
+            <h2>Meme Generator</h2>
+            <h3>Project 3</h3>
+        </header>
+    )
+}
+
+export default MemeHeader;
