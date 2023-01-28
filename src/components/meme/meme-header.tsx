b42d0d15d7cbe09463266acf7fd00c40036bd9dd
Travis Craft <tlcraft@users.noreply.github.com>
2023-01-27 17:17:14 -0700

    Add initial css file.

--- src/components/meme/meme-header.tsx
+++ src/components/meme/meme-header.tsx
@@ -1,8 +1,9 @@
+import './meme-header.css';
 import React from 'react';
 
 function MemeHeader() {
     return (
-        <header>
+        <header className="meme-header">
             <h2>Meme Generator</h2>
             <h3>Project 3</h3>
         </header>
