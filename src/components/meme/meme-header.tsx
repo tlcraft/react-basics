9858f40c9188b34d3bfc2930c95e0e061bf89a6c
Travis Craft <tlcraft@users.noreply.github.com>
2023-01-30 19:25:16 -0700

    Add margin.

--- src/components/meme/meme-header.tsx
+++ src/components/meme/meme-header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function MemeHeader() {
     return (
         <header className="meme-header">
-            <h2>Meme Generator</h2>
+            <h2 className="meme-title">Meme Generator</h2>
             <h3>Project 3</h3>
         </header>
     )
