58fe1b14b4a3ff294269a61e524ed0f3e43297c4
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-01 16:56:24 -0700

    Style project.

--- src/components/meme/meme-header.tsx
+++ src/components/meme/meme-header.tsx
@@ -5,7 +5,7 @@ function MemeHeader() {
     return (
         <header className="meme-header">
             <h2 className="meme-title">Meme Generator</h2>
-            <h3>Project 3</h3>
+            <h3 className="meme-project">Project 3</h3>
         </header>
     )
 }
