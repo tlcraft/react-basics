8dca59d3242acf1606d1607a722591bcd225442c
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-07 18:09:13 -0700

    Adjust form.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 function MemeInput() {
     return (
-        <div className="meme-input">
-            <input type="text" id="upper-text" name="upper-text" />
-            <input type="text" id="lower-text" name="lower-text" />
+        <form className="meme-input-form">
+            <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" />
+            <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" />
             <button type="button">Get a new image</button>
-        </div>
+        </form>
     )
 }
 
