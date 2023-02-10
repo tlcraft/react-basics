c08578c1070018d4fc6068ca51800ca1941c74c8
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-10 16:12:33 -0700

    Add initial click handler.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 function MemeInput() {
     return (
-        <form className="meme-input-form">
+        <div className="meme-input-form">
             <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" />
             <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" />
-            <button type="button">Get a new image</button>
-        </form>
+            <button type="button" onClick={getNewImage}>Get a new image</button>
+        </div>
     )
+}
+
+function getNewImage(): void {
+    console.log("Clicked button.");
 }
 
 export default MemeInput;
