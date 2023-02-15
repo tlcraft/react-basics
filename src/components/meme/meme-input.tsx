d04c842f5ed40900f78fcab0e768587a23ff5bad
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-14 19:56:05 -0700

    Get random image.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -1,14 +1,16 @@
 import './meme-input.css';
-import React from 'react';
+import React, { useState } from 'react';
 import memeData from './meme-data';
 
 function MemeInput() {
+    const [imageUrl, setImage] = useState(getNewImage());
+
     return (
         <div className="meme-input-form">
             <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" />
             <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" />
-            <button type="button" onClick={getNewImage}>Get a new image</button>
-            <img className="meme-image" src={getNewImage()} alt="A random meme image" />
+            <button type="button" onClick={() => setImage(getNewImage)}>Get a new image</button>
+            <img className="meme-image" src={imageUrl} alt="A random meme image" />
         </div>
     )
 }
