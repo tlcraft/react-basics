d7c1b14ae5b7e22fe6ac095a08a1e80e86bca94e
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-15 19:25:21 -0700

    Update naming.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 import memeData from './meme-data';
 
 function MemeInput() {
-    const [imageUrl, setImage] = useState(getNewImage());
+    const [imageUrl, setImageUrl] = useState(getNewImageUrl());
 
     return (
         <div className="meme-input-form">
             <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" />
             <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" />
-            <button type="button" onClick={() => setImage(getNewImage)}>Get a new image</button>
+            <button type="button" onClick={() => setImageUrl(getNewImageUrl)}>Get a new image</button>
             <img className="meme-image" src={imageUrl} alt="A random meme image" />
         </div>
     )
 }
 
-function getNewImage(): string {
+function getNewImageUrl(): string {
     const random = Math.floor(Math.random() * memeData.data.memes.length);
     return memeData.data.memes[random].url;
 }
