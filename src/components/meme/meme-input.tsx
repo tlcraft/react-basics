1dc0bcde06bef9da6cc0f2bfc1fab7870b552a4d
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-18 16:53:54 -0700

    Add state for input.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -4,15 +4,17 @@ import memeData from './meme-data';
 
 function MemeInput() {
     const [imageUrl, setImageUrl] = useState(getNewImageUrl());
-
+    const [upperText, setUpperText] = useState('');
+    const [lowerText, setLowerText] = useState('');
+    
     return (
         <>
             <div className="meme-input-form">
-                <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" />
-                <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" />
+                <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" onChange={(e) => {setUpperText(e.target.value)}}/>
+                <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" onChange={(e) => {setLowerText(e.target.value)}}/>
                 <button type="button" onClick={() => setImageUrl(getNewImageUrl)} className="new-image-btn">Get a new image</button>
             </div>
-            <img className="meme-image" src={imageUrl} alt="A random meme image" />    
+            <img className="meme-image" src={imageUrl} alt="A random meme image" />
         </>
     )
 }
