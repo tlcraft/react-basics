c22459ab01dcd7d7185de3893e5df6ea86afa8f6
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-24 14:08:23 -0700

    Use name as alt text.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -2,8 +2,17 @@ import './meme-input.css';
 import React, { useState } from 'react';
 import memeData from './meme-data';
 
+interface MemeImage {
+    id: string;
+    name: string;
+    url: string;
+    width: number;
+    height: number;
+    box_count: number;
+}
+
 function MemeInput() {
-    const [imageUrl, setImageUrl] = useState(getNewImageUrl());
+    const [image, setImage] = useState(getNewImage());
     const [upperText, setUpperText] = useState('');
     const [lowerText, setLowerText] = useState('');
 
@@ -12,10 +21,10 @@ function MemeInput() {
             <div className="meme-input-form">
                 <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" onChange={(e) => {setUpperText(e.target.value)}}/>
                 <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" onChange={(e) => {setLowerText(e.target.value)}}/>
-                <button type="button" onClick={() => setImageUrl(getNewImageUrl)} className="new-image-btn">Get a new image</button>
+                <button type="button" onClick={() => setImage(getNewImage)} className="new-image-btn">Get a new image</button>
             </div>
             <div className="meme-container">
-                <img className="meme-image" src={imageUrl} alt="A random meme image" />
+                <img className="meme-image" src={image.url} alt={image.name} />
                 <p className="meme-text upper-text">{upperText}</p>
                 <p className="meme-text lower-text">{lowerText}</p>
             </div>
@@ -23,9 +32,9 @@ function MemeInput() {
     )
 }
 
-function getNewImageUrl(): string {
+function getNewImage(): MemeImage {
     const random = Math.floor(Math.random() * memeData.data.memes.length);
-    return memeData.data.memes[random].url;
+    return memeData.data.memes[random];
 }
 
 export default MemeInput;
