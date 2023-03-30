903b59c8788c7bdc692ac30772acfcf0a1588287
Travis Craft <tlcraft@users.noreply.github.com>
2023-03-30 17:06:58 -0600

    Add list to page.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ScratchPad() {
+    const [items, setItems] = useState(["One", "Two"]);
+    const formattedItems = items.map(item => <li>{item}</li>);
     return (
-        <h1>Scratch Pad for React Concepts</h1>
+        <>
+            <h1>Scratch Pad for React Concepts</h1>
+            <ul>
+                {formattedItems}
+            </ul>
+        </>
+
     )
 }
 
