c5a498c2325e655f7b1afda2383152924454df45
Travis Craft <tlcraft@users.noreply.github.com>
2023-03-31 19:18:56 -0600

    Add items to list.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -1,12 +1,18 @@
+import './scratch-pad.css';
 import React, { useState } from 'react';
 
 function ScratchPad() {
-    const [items, setItems] = useState(["One", "Two"]);
+    const [items, setItems] = useState([1, 2]);
     const formattedItems = items.map(item => <li>{item}</li>);
+    const addToList = () => {
+        setItems(previousList => [...previousList, previousList.length + 1]);
+    };
+
     return (
         <>
             <h1>Scratch Pad for React Concepts</h1>
-            <ul>
+            <button onClick={addToList} type="button">Add To List</button>
+            <ul className='item-list'>
                 {formattedItems}
             </ul>
         </>
