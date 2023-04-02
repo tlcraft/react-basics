8acffd110f98fb392b201b0330c0a5b3312ddb0b
Travis Craft <tlcraft@users.noreply.github.com>
2023-04-01 21:05:47 -0600

    Remove defaults.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -2,7 +2,7 @@ import './scratch-pad.css';
 import React, { useState } from 'react';
 
 function ScratchPad() {
-    const [items, setItems] = useState([1, 2]);
+    const [items, setItems] = useState([] as number[]);
     const formattedItems = items.map(item => <li>{item}</li>);
     const addToList = () => {
         setItems(previousList => [...previousList, previousList.length + 1]);
