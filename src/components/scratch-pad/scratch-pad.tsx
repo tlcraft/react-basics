e318c655749cad3203edd9521558eb006169b5d1
Travis Craft <tlcraft@users.noreply.github.com>
2023-04-04 21:48:25 -0600

    Add disable method.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -8,10 +8,12 @@ function ScratchPad() {
         setItems(previousList => [...previousList, previousList.length + 1]);
     };
 
+    const isDisabled = () => { return items.length >= 10; }
+
     return (
         <>
             <h1>Scratch Pad for React Concepts</h1>
-            <button onClick={addToList} type="button">Add To List</button>
+            <button onClick={addToList} type="button" disabled={isDisabled()}>Add To List</button>
             <ul className='item-list'>
                 {formattedItems}
             </ul>
