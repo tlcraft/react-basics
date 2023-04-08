b689056b61691aa2ae1ce5ffb4c5c47865743807
Travis Craft <tlcraft@users.noreply.github.com>
2023-04-07 19:09:26 -0600

    Add remove from list button.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -8,12 +8,23 @@ function ScratchPad() {
         setItems(previousList => [...previousList, previousList.length + 1]);
     };
 
+    const removeFromList = () => {
+        setItems(previousList => {
+            if (previousList.length) {
+                return previousList.slice(0, previousList.length - 1);
+            }
+
+            return [];
+        });
+    };
+
     const isDisabled = () => { return items.length >= 10; }
 
     return (
         <>
             <h1>Scratch Pad for React Concepts</h1>
             <button onClick={addToList} type="button" disabled={isDisabled()}>Add To List</button>
+            <button onClick={removeFromList} type="button" disabled={items.length === 0}>Remove From List</button>
             <ul className='item-list'>
                 {formattedItems}
             </ul>
