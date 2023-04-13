3ce25cb49eec24797b72be39423602575bf76aa0
Travis Craft <tlcraft@users.noreply.github.com>
2023-04-12 19:21:05 -0600

    Add types, set meme list state.

--- src/components/meme/meme-input.tsx
+++ src/components/meme/meme-input.tsx
@@ -1,15 +1,6 @@
 import './meme-input.css';
 import React, { useState } from 'react';
-import memeData from './meme-data';
-
-interface MemeImage {
-    id: string;
-    name: string;
-    url: string;
-    width: number;
-    height: number;
-    box_count: number;
-}
+import { memeResponse, MemeResponse, MemeImage } from './meme-data';
 
 interface Meme {
     image: MemeImage;
@@ -18,14 +9,16 @@ interface Meme {
 }
 
 function MemeInput() {
-    const [meme, setMeme] = useState({ image: getNewImage(), upperText: '', lowerText: '' } as Meme);
+    const [memeList, setMemeList] = useState(memeResponse);
+    const [meme, setMeme] = useState({ image: getNewImage(memeList), upperText: '', lowerText: '' } as Meme);
+
 
     return (
         <>
             <div className="meme-input-form">
                 <input type="text" id="upper-text" placeholder="Upper text" name="upper-text" onChange={(e) => {setMeme(previousState => { return { ...previousState, upperText: e.target.value } } )}}/>
                 <input type="text" id="lower-text" placeholder="Lower text" name="lower-text" onChange={(e) => {setMeme(previousState => { return { ...previousState, lowerText: e.target.value } } )}}/>
-                <button type="button" onClick={() => setMeme(previousState => { return { ...previousState, image: getNewImage() } } ) } className="new-image-btn">Get a new image</button>
+                <button type="button" onClick={() => setMeme(previousState => { return { ...previousState, image: getNewImage(memeList) } } ) } className="new-image-btn">Get a new image</button>
             </div>
             <div className="meme-container">
                 <img className="meme-image" src={meme.image.url} alt={meme.image.name} />
@@ -36,9 +29,9 @@ function MemeInput() {
     )
 }
 
-function getNewImage(): MemeImage {
-    const random = Math.floor(Math.random() * memeData.data.memes.length);
-    return memeData.data.memes[random];
+function getNewImage(memeList: MemeResponse): MemeImage {
+    const random = Math.floor(Math.random() * memeList.data.memes.length);
+    return memeList.data.memes[random];
 }
 
 export default MemeInput;
