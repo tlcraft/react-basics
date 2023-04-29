d945543710002c17abcd34ebe40377cc779bacf5
Travis Craft <tlcraft@users.noreply.github.com>
2023-04-29 14:48:57 -0600

    Toggle box state.

--- src/components/scratch-pad/box.tsx
+++ src/components/scratch-pad/box.tsx
@@ -1,10 +1,11 @@
 import './box.css';
-import React from 'react';
+import React, { useState }  from 'react';
 
 export interface BoxProps {
     on: boolean;
 }
 
 export function Box(props: BoxProps) {
-    return <div className={`square ${props.on ? "yellow" : "grey"}`}></div>
+    const [isOn, setIsOn] = useState(props.on);
+    return <div className={`square ${isOn ? "yellow" : "grey"}`} onClick={() => setIsOn(prevValue => !prevValue)}></div>
 }
