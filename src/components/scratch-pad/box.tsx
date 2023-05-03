fdeeef235c3b9bc444cf6fcc88bb467085e5f933
Travis Craft <tlcraft@users.noreply.github.com>
2023-05-02 21:46:49 -0600

    Remove toggleOn method.

--- src/components/scratch-pad/box.tsx
+++ src/components/scratch-pad/box.tsx
@@ -9,7 +9,5 @@ export interface BoxProps {
 export function Box(props: BoxProps) {
     const [isOn, setIsOn] = useState(props.on);
 
-    const toggleOn = () => setIsOn(prevValue => !prevValue);
-
     return <div className={`square ${isOn ? "yellow" : "grey"}`} onClick={props.toggle}></div>
 }
