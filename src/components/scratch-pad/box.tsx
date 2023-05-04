3a7a8a4293fc848583600a4708d6be8519ba03cb
Travis Craft <tlcraft@users.noreply.github.com>
2023-05-04 13:45:55 -0600

    Clean up box.

--- src/components/scratch-pad/box.tsx
+++ src/components/scratch-pad/box.tsx
@@ -1,13 +1,12 @@
 import './box.css';
-import React, { useState }  from 'react';
+import React from 'react';
 
 export interface BoxProps {
+    id: number;
     on: boolean;
-    toggle: () => void;
+    toggle: (id: number) => void;
 }
 
 export function Box(props: BoxProps) {
-    const [isOn, setIsOn] = useState(props.on);
-
-    return <div className={`square ${isOn ? "yellow" : "grey"}`} onClick={props.toggle}></div>
+    return <div className={`square ${props.on ? "yellow" : "grey"}`} onClick={() => props.toggle(props.id)}></div>
 }
