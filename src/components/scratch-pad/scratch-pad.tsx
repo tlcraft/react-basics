47ddfaafcca6e00abb808050838f7ce846a5b41c
Travis Craft <tlcraft@users.noreply.github.com>
2023-05-05 19:49:40 -0600

    Initial toggle logic.

--- src/components/scratch-pad/scratch-pad.tsx
+++ src/components/scratch-pad/scratch-pad.tsx
@@ -1,12 +1,27 @@
 import './scratch-pad.css';
 import React, { useState } from 'react';
-import { Box, BoxProps } from './box';
-import boxes from './boxes';
+import { Box } from './box';
+import { boxes } from './boxes';
 
 function ScratchPad() {
     const [squares, setSquares] = useState(boxes);
-    const toggle = () => console.log("Clicked!");
-    const squareElements = squares.map(square => <Box key={square.id} on={ square.on } toggle={toggle} />);
+    const toggle = (boxId: number) => { 
+        setSquares(prevValue => {
+            for(let index = 0; index < prevValue.length; index++) {
+                const square = prevValue[index];
+                if (square.id === boxId) {
+                    const priorItems = prevValue.slice(0, index);
+                    const remainingItems = (index + 1) < prevValue.length ? prevValue.slice(index + 1, prevValue.length) : [];
+                    const updatedList = [...priorItems, { ...square, on: !square.on }, ...remainingItems];
+                    return updatedList;
+                }
+            };
+            
+            console.warn("Item not found!");
+            return prevValue;
+        });
+    };
+    const squareElements = squares.map(square => <Box key={square.id} id={square.id} on={ square.on } toggle={toggle} />);
 
     const [items, setItems] = useState([] as number[]);
     const formattedItems = items.map((item, index) => <li key={index}>{item}</li>);
