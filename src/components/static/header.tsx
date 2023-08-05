21d67e843608d96ccd35eea1f1e0e6587405842b
Travis Craft <tlcraft@users.noreply.github.com>
2023-08-05 15:30:09 -0600

    Add space.

--- src/components/static/header.tsx
+++ src/components/static/header.tsx
@@ -15,7 +15,7 @@ interface HeaderProps {
 
 function Header(props: HeaderProps) {
     return (
-        <nav className={props.darkMode ? "dark main-nav": "main-nav"}>
+        <nav className={props.darkMode ? "dark main-nav" : "main-nav"}>
             <img src={logo} className="App-logo" alt="logo" />
             <h3>React Sample App</h3>
             <ul className="nav-items">
