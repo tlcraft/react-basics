59455d970259417aac7177ce91e4c6df15f3bb43
Travis Craft <tlcraft@users.noreply.github.com>
2023-11-07 18:40:25 -0700

    Add style method.

--- src/components/tenzies/die.tsx
+++ src/components/tenzies/die.tsx
@@ -9,8 +9,28 @@ export interface DieProps {
 
 function Die(props: DieProps) {
     const { die } = props;
+
+    const getDieStyle = () => {
+        switch(props.die.value) {
+            case 1:
+                return 'one';
+            case 2:
+                return 'two';
+            case 3:
+                return 'three';
+            case 4:
+                return 'four';
+            case 5: 
+                return 'five';
+            case 6:
+                return 'six';
+            default:
+                throw new Error("Value not defined.");
+        }
+    };
+
     return (
-        <div className={die.hold ? 'hold die' : 'die'} onClick={() => props.save()}>{die.value}</div>
+        <div className={die.hold ? `hold die ${getDieStyle()}` : `die ${getDieStyle()}`} onClick={() => props.save()}>{die.value}</div>
     )
 }
 
